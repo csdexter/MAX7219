190eb757dd634355d8bcc31b4d25d6d5394e1adc
Radu - Eosif Mihailescu <radu.mihailescu@linux360.ro>
2012-04-21 19:24:53 +0100

    Added Matrix example
    - sorted matrix orientation out, updated documentation
    - added matrix display example

--- examples/MAX7219_Matrix/MAX7219_Matrix.ino
+++ examples/MAX7219_Matrix/MAX7219_Matrix.ino
@@ -1,162 +1,205 @@
-//We always have to include the library
-#include <LedControl.h>
-
 /*
- Now we need a LedControl to work with.
- ***** These pin numbers will probably not work with your hardware *****
- pin 12 is connected to the DataIn 
- pin 11 is connected to the CLK 
- pin 10 is connected to LOAD 
- We have only a single MAX72XX.
- */
-LedControl lc=LedControl(12,11,10,1);
+* MAX7219 Matrix Example Sketch
+* Written by Radu - Eosif Mihailescu
+*
+* This example sketch illustrates how to use some of the basic commands in the
+* MAX7219 Library. The sketch will use the MAX7219/7221 to display some
+* graphics on an 8x8 dot-matrix display.
+* More information on the MAX7219/7221 chips can be found in the datasheet.
+*
+* HARDWARE SETUP:
+* This sketch assumes you have connected your MAX7219/7221 to the SPI
+* interface of the Arduino as explained in the README file. An 8x8 dot-matrix
+* display should be connected to the MAX7219/7221: each common cathode rail of
+* the matrix to the corresponding DIG input of the MAX7219/7221 and every
+* common anode rail to the corresponding SEG output of the MAX7219/7221.
+* Physically orient the matrix such that the topmost row of pixels is the one
+* connected to the DIG0 input and the leftmost column of pixels is the one
+* connected to the SEGDP output. Finally, a current limiting resistor should
+* be connected between ISET and V+, whose value can be determined from 
+* MAX7219's and the matrix' datasheets.
+* 
+* MATRIX WIRING DIAGRAM:
+* For those of you who appreciate images more than words, here it goes:
+*
+* DIG0 -*--------*--------*--------*--------*--------*--------*--------\
+*       |        |        |        |        |        |        |        |
+*       \-|<|-\  \-|<|-\  \-|<|-\  \-|<|-\  \-|<|-\  \-|<|-\  \-|<|-\  \-|<|-\
+*             |        |        |        |        |        |        |        |
+* DIG1 -*-----+--*-----+--*-----+--*-----+--*-----+--*-----+--*-----+--*-----+
+*       |     |  |     |  |     |  |     |  |     |  |     |  |     |  |     |
+*       \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-* 
+*             |        |        |        |        |        |        |        |
+* DIG3 -*-----+--*-----+--*-----+--*-----+--*-----+--*-----+--*-----+--*-----+
+*       |     |  |     |  |     |  |     |  |     |  |     |  |     |  |     |
+*       \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-* 
+*             |        |        |        |        |        |        |        |
+* DIG4 -*-----+--*-----+--*-----+--*-----+--*-----+--*-----+--*-----+--*-----+
+*       |     |  |     |  |     |  |     |  |     |  |     |  |     |  |     |
+*       \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-* 
+*             |        |        |        |        |        |        |        |
+* DIG5 -*-----+--*-----+--*-----+--*-----+--*-----+--*-----+--*-----+--*-----+
+*       |     |  |     |  |     |  |     |  |     |  |     |  |     |  |     |
+*       \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-* 
+*             |        |        |        |        |        |        |        |
+* DIG6 -*-----+--*-----+--*-----+--*-----+--*-----+--*-----+--*-----+--*-----+
+*       |     |  |     |  |     |  |     |  |     |  |     |  |     |  |     |
+*       \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-* 
+*             |        |        |        |        |        |        |        |
+* DIG7 -*-----+--*-----+--*-----+--*-----+--*-----+--*-----+--*-----+--*-----+
+*       |     |  |     |  |     |  |     |  |     |  |     |  |     |  |     |
+*       \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-* 
+*             |        |        |        |        |        |        |        |
+* DIG7 -*-----+--*-----+--*-----+--*-----+--*-----+--*-----+--*-----+--*-----+
+*       |     |  |     |  |     |  |     |  |     |  |     |  |     |  |     |
+*       \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-*  \-|<|-* 
+*             |        |        |        |        |        |        |        |
+*          SEGDP    SEGA     SEGB     SEGC     SEGD     SEGE     SEGF     SEGG
+*
+* USING THE SKETCH:
+* Compile, upload, enjoy :-)
+*
+*/
 
+//Due to a bug in Arduino, this needs to be included here too/first
+#include <SPI.h>
+
+#include <MAX7219.h>
+
+const MAX7219_Topology topology = {MAX7219_MODE_MATRIX, 0, 0, 0, 7};
 /* we always wait a bit between updates of the display */
-unsigned long delaytime=100;
+const byte delaytime = 250;
+#define MOVIE_LENGTH 7
+/* the short animation we will display */
+const byte movie[MOVIE_LENGTH][8] = {{B00111100,
+                                      B00111100,
+                                      B00011000,
+                                      B00011000,
+                                      B00011000,
+                                      B00011000,
+                                      B00111100,
+                                      B00111100},
+                                     {B00000000,
+                                      B01100110,
+                                      B11111111,
+                                      B11111111,
+                                      B01111110,
+                                      B00111100,
+                                      B00011000,
+                                      B00000000},
+                                     {B01100000,
+                                      B10010000,
+                                      B11110000,
+                                      B10010000,
+                                      B10011011,
+                                      B00001100,
+                                      B00001000,
+                                      B00001000},
+                                     {B00010000,
+                                      B00010000,
+                                      B01110000,
+                                      B10010000,
+                                      B01010000,
+                                      B00001001,
+                                      B00001011,
+                                      B00000101},
+                                     {B00000000,
+                                      B00011000,
+                                      B00000000,
+                                      B00111000,
+                                      B00011000,
+                                      B00011000,
+                                      B01111110,
+                                      B01111110},
+                                     {B00000000,
+                                      B10100000,
+                                      B11010000,
+                                      B10010000,
+                                      B00000110,
+                                      B00001001,
+                                      B00001001,
+                                      B00000110},
+                                     {B00111100,
+                                      B00111100,
+                                      B00111100,
+                                      B00011000,
+                                      B00000000,
+                                      B00011000,
+                                      B00111100,
+                                      B00011000}};
+/* the first element is an operation code and the second is the number of
+   animation ticks to perform it for:
+  - opcode 1: display frame
+  - opcode 2: flash frame */
+const byte script[MOVIE_LENGTH][2] = {{1, 4}, {2, 4}, {1, 2}, {1, 2}, {1, 2},
+                                      {1, 2}, {1, 8}};
+
+MAX7219 maxled;
+byte framebuffer[8];
 
 void setup() {
-  /*
-   The MAX72XX is in power-saving mode on startup,
-   we have to do a wakeup call
-   */
-  lc.shutdown(0,false);
-  /* Set the brightness to a medium values */
-  lc.setIntensity(0,8);
-  /* and clear the display */
-  lc.clearDisplay(0);
+  maxled.begin(&topology);
+  maxled.zeroDisplay();
 }
 
-/*
- This method will display the characters for the
- word "Arduino" one after the other on the matrix. 
- (you need at least 5x7 leds to see the whole chars)
- */
-void writeArduinoOnMatrix() {
-  /* here is the data for the characters */
-  byte a[5]={B01111110,B10001000,B10001000,B10001000,B01111110};
-  byte r[5]={B00111110,B00010000,B00100000,B00100000,B00010000};
-  byte d[5]={B00011100,B00100010,B00100010,B00010010,B11111110};
-  byte u[5]={B00111100,B00000010,B00000010,B00000100,B00111110};
-  byte i[5]={B00000000,B00100010,B10111110,B00000010,B00000000};
-  byte n[5]={B00111110,B00010000,B00100000,B00100000,B00011110};
-  byte o[5]={B00011100,B00100010,B00100010,B00100010,B00011100};
-
-  /* now display them one by one with a small delay */
-  lc.setRow(0,0,a[0]);
-  lc.setRow(0,1,a[1]);
-  lc.setRow(0,2,a[2]);
-  lc.setRow(0,3,a[3]);
-  lc.setRow(0,4,a[4]);
-  delay(delaytime);
-  lc.setRow(0,0,r[0]);
-  lc.setRow(0,1,r[1]);
-  lc.setRow(0,2,r[2]);
-  lc.setRow(0,3,r[3]);
-  lc.setRow(0,4,r[4]);
-  delay(delaytime);
-  lc.setRow(0,0,d[0]);
-  lc.setRow(0,1,d[1]);
-  lc.setRow(0,2,d[2]);
-  lc.setRow(0,3,d[3]);
-  lc.setRow(0,4,d[4]);
-  delay(delaytime);
-  lc.setRow(0,0,u[0]);
-  lc.setRow(0,1,u[1]);
-  lc.setRow(0,2,u[2]);
-  lc.setRow(0,3,u[3]);
-  lc.setRow(0,4,u[4]);
-  delay(delaytime);
-  lc.setRow(0,0,i[0]);
-  lc.setRow(0,1,i[1]);
-  lc.setRow(0,2,i[2]);
-  lc.setRow(0,3,i[3]);
-  lc.setRow(0,4,i[4]);
-  delay(delaytime);
-  lc.setRow(0,0,n[0]);
-  lc.setRow(0,1,n[1]);
-  lc.setRow(0,2,n[2]);
-  lc.setRow(0,3,n[3]);
-  lc.setRow(0,4,n[4]);
-  delay(delaytime);
-  lc.setRow(0,0,o[0]);
-  lc.setRow(0,1,o[1]);
-  lc.setRow(0,2,o[2]);
-  lc.setRow(0,3,o[3]);
-  lc.setRow(0,4,o[4]);
-  delay(delaytime);
-  lc.setRow(0,0,0);
-  lc.setRow(0,1,0);
-  lc.setRow(0,2,0);
-  lc.setRow(0,3,0);
-  lc.setRow(0,4,0);
-  delay(delaytime);
+void animateArduino() {
+  for(byte i = 0; i < MOVIE_LENGTH; i++) {
+    switch(script[i][0]) {
+      case 1:
+        maxled.setMatrix(movie[i]);
+        delay(delaytime * script[i][1]);
+      break;
+      case 2:
+        for(byte j = 0; j < script[i][1]; j++) {
+          maxled.setMatrix(movie[i]);
+          delay(delaytime / 2);
+          maxled.clearDisplay();
+          delay(delaytime / 2);
+        }
+      break;  
+    }
+  }
 }
 
-/*
-  This function lights up a some Leds in a row.
- The pattern will be repeated on every row.
- The pattern will blink along with the row-number.
- row number 4 (index==3) will blink 4 times etc.
- */
 void rows() {
-  for(int row=0;row<8;row++) {
-    delay(delaytime);
-    lc.setRow(0,row,B10100000);
-    delay(delaytime);
-    lc.setRow(0,row,(byte)0);
-    for(int i=0;i<row;i++) {
-      delay(delaytime);
-      lc.setRow(0,row,B10100000);
-      delay(delaytime);
-      lc.setRow(0,row,(byte)0);
+  for(byte i = 0; i < 7; i++) {
+    framebuffer[i] = 0xFF;
+    for(byte j = 0; j < 4; j++) {
+      maxled.setMatrix(framebuffer);
+      delay(delaytime / 2);
+      maxled.clearDisplay();
+      delay(delaytime / 2);      
+    }
+    framebuffer[i] = 0x00;
+  }
+}
+
+void columns(){
+  for(byte i = 0; i < 7; i++) {
+    memset(framebuffer, 1 << i, 8);
+    for(byte j = 0; j < 4; j++) {
+      maxled.setMatrix(framebuffer);
+      delay(delaytime / 2);
+      maxled.clearDisplay();
+      delay(delaytime / 2);      
     }
   }
 }
 
-/*
-  This function lights up a some Leds in a column.
- The pattern will be repeated on every column.
- The pattern will blink along with the column-number.
- column number 4 (index==3) will blink 4 times etc.
- */
-void columns() {
-  for(int col=0;col<8;col++) {
+void single(){
+  memset(framebuffer, 0x00, 8);
+  for(byte i = 0; i < 7; i++) {
+    framebuffer[0] = 1 << i;
+    framebuffer[i] = 0x01 | 1 << i;
+    maxled.setMatrix(framebuffer);
     delay(delaytime);
-    lc.setColumn(0,col,B10100000);
-    delay(delaytime);
-    lc.setColumn(0,col,(byte)0);
-    for(int i=0;i<col;i++) {
-      delay(delaytime);
-      lc.setColumn(0,col,B10100000);
-      delay(delaytime);
-      lc.setColumn(0,col,(byte)0);
-    }
-  }
-}
-
-/* 
- This function will light up every Led on the matrix.
- The led will blink along with the row-number.
- row number 4 (index==3) will blink 4 times etc.
- */
-void single() {
-  for(int row=0;row<8;row++) {
-    for(int col=0;col<8;col++) {
-      delay(delaytime);
-      lc.setLed(0,row,col,true);
-      delay(delaytime);
-      for(int i=0;i<col;i++) {
-        lc.setLed(0,row,col,false);
-        delay(delaytime);
-        lc.setLed(0,row,col,true);
-        delay(delaytime);
-      }
-    }
+    framebuffer[0] = 0x00;
+    framebuffer[i] = 0x00;
   }
 }
 
 void loop() { 
-  writeArduinoOnMatrix();
+  animateArduino();
   rows();
   columns();
   single();
